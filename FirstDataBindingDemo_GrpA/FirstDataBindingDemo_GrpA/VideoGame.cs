1de44bbac05e5934717cd2d107e96b0dd766fd4e
Shizlers <w0434675@nscc.ca>
2022-04-22 14:20:21 -0300

    As best I can do, it seems like this has different tools than what I used before.

---
+++ FirstDataBindingDemo_GrpA/FirstDataBindingDemo_GrpA/VideoGame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalNote
+{
+    public class VideoGame
+    {
+        public string GameName { get; set; }
+       
+
+        public VideoGame()      //Constructor
+        {
+            this.GameName = "Starcraft";
+            
+        }
+
+        public string Summary
+        {
+            get
+            {
+                return $"{this.GameName}"; 
+            }
+        }
+
+        public VideoGame(string words)
+        {
+            this.GameName = "Starcraft";
+           
+        }
+
+    }
+}
---
+++ FirstDataBindingDemo_GrpA/FirstDataBindingDemo_GrpA/VideoGameData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalNote
+{
+    public class VideoGameData
+    {
+        //private VideoGame aGame = new VideoGame();
+        //public VideoGame AGame { get { return this.aGame; } }
+
+        private ObservableCollection<VideoGame> games = new ObservableCollection<VideoGame>();
+        public ObservableCollection<VideoGame> Games { get { return this.games; } }
+        public VideoGameData()
+        {
+            this.games.Add(new VideoGame()
+            {
+                GameName = "Quake II"
+                
+            });
+
+            games.Add(new VideoGame()
+            {
+                GameName = "Doc 1"
+            });
+
+            this.games.Add(new VideoGame()
+            {
+                GameName = "Doc 2"
+            });
+
+            this.games.Add(new VideoGame()
+            {
+                GameName = "Doc3"
+            });
+        }
+        public void addGame()
+        {
+            this.games.Add(new VideoGame()
+            {
+                GameName = "Doc4"
+                
+            });
+        }
+    }
+}
